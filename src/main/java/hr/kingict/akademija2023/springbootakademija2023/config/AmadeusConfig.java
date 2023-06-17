8436d80b0623f52ada56ff1c81f827cfcb9cf053
Sanjin Hadžalić <sh14.sanjin@yahoo.com>
2023-06-17 16:04:31 +0200

    feat: add bunch of stuff

---
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/config/AmadeusConfig.java
@@ -0,0 +1,13 @@
+package hr.kingict.akademija2023.springbootakademija2023.config;
+
+import com.amadeus.Amadeus;
+import org.springframework.context.annotation.Bean;
+
+public class AmadeusConfig {
+    @Bean
+    public Amadeus getAmadeus(){
+        return Amadeus
+                .builder("RuRNwWDrX0F4G3nLKVGDGEZiOPGoIlew", "jOHSRFlXPsGwDFyz")
+                .build();
+    }
+}
