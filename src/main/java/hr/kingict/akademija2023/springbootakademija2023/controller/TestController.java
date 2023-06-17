0ce7e643370767fdb3c1f48f9091672a9c1250fe
Sanjin Hadžalić <sh14.sanjin@yahoo.com>
2023-06-17 15:34:41 +0200

    feat: add bunch of stuff

--- src/main/java/hr/kingict/akademija2023/springbootakademija2023/controller/TestController.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/controller/TestController.java
@@ -1,10 +1,10 @@
 package hr.kingict.akademija2023.springbootakademija2023.controller;
 
+import hr.kingict.akademija2023.springbootakademija2023.dto.TestDto;
+import hr.kingict.akademija2023.springbootakademija2023.form.TestForm;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/test")
@@ -25,4 +25,27 @@ public class TestController {
                 .header("testniHeader", "blabla")
                 .body("Greska");
     }
+    @GetMapping("/getDto")
+    public ResponseEntity<TestDto> testGetDto(){
+        TestDto testDto = new TestDto();
+        testDto.setName("Ime");
+        testDto.setDescription("opis");
+
+        return ResponseEntity
+                .ok()
+                .body(testDto);
+    }
+    @PostMapping(value = "/post")
+    public ResponseEntity<TestDto> testPostWithParam(@RequestBody @Valid TestForm testForm){
+
+        TestDto testDto = new TestDto();
+        testDto.setName(testForm.getName());
+        testDto.setDescription(testForm.getDescription());
+
+        return ResponseEntity
+                .ok()
+                .body(testDto);
+    }
+
+
 }
