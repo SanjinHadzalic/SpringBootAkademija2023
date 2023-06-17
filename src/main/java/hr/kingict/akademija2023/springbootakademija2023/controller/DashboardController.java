aa2864db97cc81fdd074afb50c53a365734d7a0f
Sanjin Hadžalić <sh14.sanjin@yahoo.com>
2023-06-17 11:27:49 +0200

    feat: add methods in DashboardController, DashboardServiceProduction.java, DashboardServiceTest

--- src/main/java/hr/kingict/akademija2023/springbootakademija2023/controller/DashboardController.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/controller/DashboardController.java
@@ -1,13 +1,17 @@
 package hr.kingict.akademija2023.springbootakademija2023.controller;
 
+import hr.kingict.akademija2023.springbootakademija2023.services.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+    @Autowired
+    private DashboardService dashboardService;
 
     @GetMapping(value = "/dashboard")
     public String getDashboard(){
-        return "dashboard.html";
+        return dashboardService.getDashboard();
     }
 }
--- src/main/java/hr/kingict/akademija2023/springbootakademija2023/impl/DashboardServiceProduction.java
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/impl/DashboardServiceProduction.java
@@ -1,9 +1,12 @@
 package hr.kingict.akademija2023.springbootakademija2023.impl;
 
 import hr.kingict.akademija2023.springbootakademija2023.services.DashboardService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
+@Profile("prod")
 public class DashboardServiceProduction implements DashboardService {
     @Override
     public String getDashboard() {
