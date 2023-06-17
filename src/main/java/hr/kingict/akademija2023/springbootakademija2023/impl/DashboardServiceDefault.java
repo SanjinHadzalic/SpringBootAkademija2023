67b220728428e86eb2829af00ab7f3f7b460950c
Sanjin Hadžalić <sh14.sanjin@yahoo.com>
2023-06-17 11:36:51 +0200

    feat: add DashboardServiceDefault

---
+++ src/main/java/hr/kingict/akademija2023/springbootakademija2023/impl/DashboardServiceDefault.java
@@ -0,0 +1,14 @@
+package hr.kingict.akademija2023.springbootakademija2023.impl;
+
+import hr.kingict.akademija2023.springbootakademija2023.services.DashboardService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("!prod & !test")
+public class DashboardServiceDefault implements DashboardService {
+    @Override
+    public String getDashboard() {
+        return "dashboard.html";
+    }
+}
